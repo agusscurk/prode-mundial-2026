3126e2e3f0eec5a82a6495251b1859f04c8d2998
agusscurk <agus.scurk@gmail.com>
2026-05-18 00:04:11 -0300

    Fix: Disable static generation with next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        dynamicIO: true,
+    },
+    typescript: {
+        ignoreBuildErrors: false,
+    },
+};
 
 export default nextConfig;
